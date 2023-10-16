7d4809219f109077611c8b21c1c50cbc5727ca1d
MartinBartos97 <martin.bartos971014@gmail.com>
2023-10-16 14:30:33 +0200

    Optimalization of database insert/get

--- WS/WS_app/Scrape.py
+++ WS/WS_app/Scrape.py
@@ -12,15 +12,15 @@ def scrape():
         car_offer = parsed_source.find("div", class_="maincontent").find_all("div", class_="inzeraty inzeratyflex")
         car_offers += car_offer
 
+    existing_instances = Car.objects.values_list("title", flat=True)
+
     car_instances = []
     for car in car_offers:
         title = car.find("div", class_="inzeratynadpis").find("h2", class_="nadpis").find("a").text
-        image_url = car.find("div", class_="inzeratynadpis").find("a").find("img").get("src")
-        price = car.find("div", class_="inzeratycena").find("b").text
-        location = car.find("div", class_="inzeratylok").text
-
-        existing_instance = Car.objects.filter(title=title).first()
-        if not existing_instance:
+        if not title in existing_instances:
+            image_url = car.find("div", class_="inzeratynadpis").find("a").find("img").get("src")
+            price = car.find("div", class_="inzeratycena").find("b").text
+            location = car.find("div", class_="inzeratylok").text
             car_instance = Car(title=title, img_url=image_url, location=location, price=price)
             car_instances.append(car_instance)
         else:
